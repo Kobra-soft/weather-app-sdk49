91c22b114e83df881e3021706931edd427ce1c0b
kobra-soft <code.softwaredev@gmail.com>
2025-02-12 23:18:47 +0000

    Commit

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+/// ACTIVITY 6
+///////////////////////////////
 
+/* import { View, StyleSheet } from "react-native";
+import Weather from "./components/Weather";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app2!</Text>
-      <StatusBar style="auto" />
+      <Weather city="London" weather="Sunny" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+});
+ */
+
+/// ACTIVITY 7
+///////////////////////////////
+
+import { View, StyleSheet } from "react-native";
+import Weather from "./components/Weather";
+export default function App() {
+  return (
+    <View style={styles.f}>
+      <Weather style={styles.f} city="London" weather="Sunny" />{" "}
+      <Weather style={styles.f} city="Bristol" weather="Sunny" />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  f: {
+    flex: 1,
   },
 });
---
+++ components/Weather.jsx
@@ -0,0 +1,41 @@
+/// ACTIVITY 6
+///////////////////////////////
+
+/* import { Text, View } from "react-native";
+const Weather = (props) => {
+  return (
+    <View>
+      <Text>
+        The weather in {props.city}, is {props.weather}.
+      </Text>
+    </View>
+  );
+};
+export default Weather;
+ */
+
+/// ACTIVITY 7
+///////////////////////////////
+
+/* import { Text, View, StyleSheet } from "react-native";
+const Weather = (props) => {
+  return (
+    <View>
+      <Text style={styles.blue}> Weather Report </Text>
+      <Text style={styles.red}>
+        The weather in {props.city}, is {props.weather}.
+      </Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  blue: {
+    color: "blue",
+    fontWeight: "bold",
+    fontSize: 30,
+  },
+  red: {
+    color: "red",
+  },
+});
+export default Weather; */
