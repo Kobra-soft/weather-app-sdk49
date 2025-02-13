d9a6856691aa01354a4331895fd88c0aef01125d
kobra-soft <code.softwaredev@gmail.com>
2025-02-13 18:06:18 +0000

    Commit

--- App.js
+++ App.js
@@ -46,7 +46,7 @@ const styles = StyleSheet.create({
 /// ACTIVITY 9
 ///////////////////////////////
 
-import { View, StyleSheet } from "react-native";
+/* import { View, StyleSheet } from "react-native";
 import Weather from "./components/Weather";
 import City from "./components/City";
 
@@ -66,4 +66,42 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+}); */
+
+/// ACTIVITY 10
+///////////////////////////////
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+var weatherReports = {};
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`API server listening on port ${port}`);
+});
+
+app.get("/weather/:city", (request, response) => {
+  cityname = request.params.city;
+  console.log(`get: City: ${cityname}`);
+  response.send({
+    city: cityname,
+    report: weatherReports[cityname],
+  });
+});
+
+app.post("/weather/:city", (request, response) => {
+  cityname = request.params.city;
+  console.log(`post: City: ${cityname}`);
+  report = request.body.report;
+  console.log(`post: Report: ${report}`);
+  weatherReports[cityname] = report;
+  response.send({
+    city: cityname,
+    report: weatherReports[cityname],
+  });
 });
